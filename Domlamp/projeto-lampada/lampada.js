db7ff6904cf813ede8aff9df7f763b8a3121cb77
Jonysson Hofeer <136395809+JoHoSantos@users.noreply.github.com>
2023-09-06 18:33:31 -0300

    q1
    q1

--- Domlamp/projeto-lampada/lampada.js
+++ Domlamp/projeto-lampada/lampada.js
@@ -1,8 +1,10 @@
 const turnOn = document.getElementById( 'turnOn' );
 const turnOff = document.getElementById( 'turnOff' );
 const lamp = document.getElementById( 'lamp' );
-const lampNew = document.getElementById('tcLamp')
-const btn = document.getElementById('btn')
+const lampNew = document.getElementById('tcLamp');
+
+
+
 
 function isLampBroken() {
     return lamp.src.indexOf('quebrada') > -1
@@ -10,33 +12,31 @@ function isLampBroken() {
 
 function lampOn() {
     if (!isLampBroken ()) {
-        lamp.src='./img/ligada.jpg';
-        
+        lamp.src='./img/ligada.png';
+        lamp.style.backgroundColor = "black";
     }
 }
 
 function lampOff() {
     if (!isLampBroken ()) {
-        lamp.src='./img/desligada.jpg';
+        lamp.src='./img/desligada.png';
+        lamp.style.backgroundColor = "cornflowerblue";
     }
 }
 
 function lampBroken(){
-    lamp.src='./img/quebrada.jpg';
+    lamp.src='./img/quebada.png';
+    lamp.style.backgroundColor = "beige";
 }
 
 function lampNova(){
-    lamp.src='./img/ligada.jpg';
+    lamp.src='./img/ligada.png';
+    lamp.style.backgroundColor = "black";
 }
-
 
 turnOn.addEventListener('click', lampOn);
 turnOff.addEventListener('click', lampOff);
 turnOn.addEventListener('dblclick',lampBroken);
 lampNew.addEventListener('click', lampNova);
-
-
-lamp.addEventListener('mouseover', lampOn);
-lamp.addEventListener('mouseleave', lampOff);
 lamp.addEventListener('dblclick',lampBroken);
 lamp.addEventListener('click',lampNova)
